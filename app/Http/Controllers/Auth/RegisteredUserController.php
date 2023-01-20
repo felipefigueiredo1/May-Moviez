bb530cc1032dcf1dad9f71adaa333c0a24d5ba75
felipefigueiredo1 <felipefigueiredosal@gmail.com>
2023-01-20 09:56:13 -0300

    Register fix

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,7 +44,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'is_admin' => $request->is_admin
+            'is_admin' => 0
         ]);
 
         event(new Registered($user));
